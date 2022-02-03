68415009defbfddf3141e3334a9d823f85c19eab
Tshep9 <91638924+Tshep9@users.noreply.github.com>
2022-02-03 10:33:16 -0500

    Arrows move and arrow boxes exist

--- ArrowBox.py
+++ ArrowBox.py
@@ -4,7 +4,7 @@ import pygame, sys, math
 class ArrowBox():
     def __init__(self, kind, startPos=[0, 0]):
         self.kind = kind
-        self.image = pygame.image.load("Arrows/ArrowTiles/Images/"+ self.kind + "Arrow.png")
+        self.image = pygame.image.load("Arrows/ArrowBox/Images/"+ self.kind + "ArrowBox.png")
         self.rect = self.image.get_rect()
         self.rad = (self.rect.height / 2 + self.rect.width / 2) / 2
         self.rect = self.rect.move(startPos)
