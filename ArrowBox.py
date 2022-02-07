1355641d9eebbd5326c699ac8958ca9a3cebae27
Tshep9 <91638924+Tshep9@users.noreply.github.com>
2022-02-07 10:34:53 -0500

    Arrow deletion and scoring works. No longer dies with index errors

--- ArrowBox.py
+++ ArrowBox.py
@@ -4,17 +4,17 @@ import pygame, sys, math
 class ArrowBox():
     def __init__(self, kind, startPos=[0, 0]):
         self.kind = kind
-        self.image = pygame.image.load("Arrows/ArrowBox/Images/"+ self.kind + "ArrowBox.png")
+        self.image = pygame.image.load("Arrows/ArrowBox/Images/" + self.kind + "ArrowBox.png")
         self.rect = self.image.get_rect()
         self.rad = (self.rect.height / 2 + self.rect.width / 2) / 2
         self.rect = self.rect.move(startPos)
 
         self.living = True
 
+        self.active = False
+
 
     def getDist(self, other):
-        x1 = self.rect.centerx
-        x2 = other.rect.centerx
         y1 = self.rect.centery
         y2 = other.rect.centery
-        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        return abs(y2-y1)
